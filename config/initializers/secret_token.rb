a2a5f8ef6aedacdde912d3ec307fa562c9bcf324
bosun adebowale <muyiwaade@yahoo.com>
2012-09-04 16:22:56 +0100

    initial file upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigbids::Application.config.secret_token = '6d0677824f487b89c6c10932fd08ddd8597de71cc6719a00a256229ed4cd1e0a370ce626785ba9813be0606ad9fd9632caeb1ea589af0e92a02ba2f115117db8'
